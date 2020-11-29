ab5afaa0afcfafaa99bfc06907b02d185f6c835e
ts-miller <tylerstephen11@gmail.com>
2020-11-29 18:51:15 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const result = array.find( function(e) { return e['result'] === 'W' })
+  return !!result ? result['year'] : result
+}
